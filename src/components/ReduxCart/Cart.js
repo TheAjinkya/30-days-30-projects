e0edaab408541ef24aeca658e88d26067b3ae389
Monali Bedre <ajinkya.chanshetty@gmail.com>
2023-12-08 23:38:10 +0000

    Updated the users component

--- src/components/ReduxCart/Cart.js
+++ src/components/ReduxCart/Cart.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
 
 function Cart() {
 
     const cartStore = useSelector(store => store.cart);
+
+    useEffect(() => {
+        fetch("https://reduxcart-62b7a-default-rtdb.firebaseio.com/abc.json", {
+            method: "POST",
+            mode: 'no-cors',
+            body: JSON.stringify(cartStore)
+        })
+        console.log("Data updated with ", cartStore)
+    }, [cartStore])
+
     return (
         <div>
             <div className="p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
